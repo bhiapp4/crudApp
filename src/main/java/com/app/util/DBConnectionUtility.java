380e08be358caf2d4ff70f6c65cb618d93ef4726
bhiapp4 <bhiapp4@gmail.com>
2017-08-15 14:38:45 -0500

    updated DB connection utility to use datasource

--- src/main/java/com/app/util/DBConnectionUtility.java
+++ src/main/java/com/app/util/DBConnectionUtility.java
@@ -7,35 +7,29 @@ import javax.sql.*;
 public class DBConnectionUtility {
 
 	public static Connection getDBConnection() {
-		
-	String DATASOURCE_CONTEXT = "java:comp/env/jdbc/crudDB";
-    
-    	Connection result = null;
-    	try {
-      		Context initialContext = new InitialContext();
-      		if ( initialContext == null){
-			log("JNDI problem. Cannot get InitialContext.");
-      		}
-      		DataSource datasource = (DataSource)initialContext.lookup(DATASOURCE_CONTEXT);
-      		if (datasource != null) {
-        		result = datasource.getConnection();
-      		}
-      		else {
-       	 		log("Failed to lookup datasource.");
-      		}
-    	}
-    	catch ( NamingException ex ) {
-     	 log("Cannot get connection: " + ex);
-    	}
-    	catch(SQLException ex){
-     	 log("Cannot get connection: " + ex);
-    	}catch(Throwable te){
-     	   te.printStackTrace();
-    	}
-    	return result;
-    }
 
-    private static void log(Object aObject){
-    	System.out.println(aObject);
-    }
+		String DATASOURCE_CONTEXT = "java:comp/env/jdbc/simpleAppDB";
+
+		Connection result = null;
+		try {
+			Context initialContext = new InitialContext();
+			DataSource datasource = (DataSource) initialContext.lookup(DATASOURCE_CONTEXT);
+			if (datasource != null) {
+				result = datasource.getConnection();
+			} else {
+				log("Failed to lookup datasource.");
+			}
+		} catch (NamingException ex) {
+			log("Cannot get connection: " + ex);
+		} catch (SQLException ex) {
+			log("Cannot get connection: " + ex);
+		} catch (Throwable te) {
+			te.printStackTrace();
+		}
+		return result;
+	}
+
+	private static void log(Object aObject) {
+		System.out.println(aObject);
+	}
 }
