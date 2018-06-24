45d5cf2ef452c61f1597bdf92f932b25737e1606
vynu <vynu@live.com>
2018-06-24 12:50:01 -0500

    empty test cases

---
+++ src/test/java/com/app/dao/StudentDaoImplTest.java
@@ -0,0 +1,28 @@
+package com.app.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentDaoImplTest {
+
+    @Test
+    public void addStudent() {
+    }
+
+    @Test
+    public void deleteStudent() {
+    }
+
+    @Test
+    public void updateStudent() {
+    }
+
+    @Test
+    public void getAllStudents() {
+    }
+
+    @Test
+    public void getStudentById() {
+    }
+}
